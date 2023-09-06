975fa0121a200386789dad68912d8709d5d794c8
Neil Khatri <79176897+nkhatri7@users.noreply.github.com>
2023-09-06 15:17:34 +1000

    initialise project

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Xpnsr</Text>
+      <Text>xpnsr</Text>
       <StatusBar style="auto" />
     </View>
   );
