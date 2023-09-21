e1415b61dfa54f2e41ddee9f3148263a87ba1dbe
Neil Khatri <79176897+nkhatri7@users.noreply.github.com>
2023-09-21 11:18:44 +1000

    feat: save theme preference locally

--- context/ThemeContext.tsx
+++ context/ThemeContext.tsx
@@ -3,10 +3,12 @@ import {
   PropsWithChildren,
   createContext,
   useContext,
+  useEffect,
   useState,
 } from "react";
 import { Theme, darkTheme, lightTheme } from "../constants/colours";
 import { Appearance } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface ThemeContextType {
   isDarkMode: boolean;
@@ -27,11 +29,24 @@ export const ThemeProvider: FC<PropsWithChildren> = ({ children }) => {
   const initialTheme = initialColourScheme === "dark" ? darkTheme : lightTheme;
   const [theme, setTheme] = useState<Theme>(initialTheme);
 
-  const toggleTheme = () => {
-    setTheme((prevTheme) => {
-      Appearance.setColorScheme(prevTheme === lightTheme ? "dark" : "light");
-      return prevTheme === lightTheme ? darkTheme : lightTheme;
-    });
+  useEffect(() => {
+    const getPreferredTheme = async () => {
+      const preferredTheme = await AsyncStorage.getItem("theme");
+      if (preferredTheme) {
+        setTheme(preferredTheme === "dark" ? darkTheme : lightTheme);
+      }
+    };
+
+    getPreferredTheme();
+  }, []);
+
+  const toggleTheme = async () => {
+    Appearance.setColorScheme(theme === lightTheme ? "dark" : "light");
+    await AsyncStorage.setItem(
+      "theme",
+      theme === lightTheme ? "dark" : "light"
+    );
+    setTheme((prevTheme) => prevTheme === lightTheme ? darkTheme : lightTheme);
   };
 
   return (
